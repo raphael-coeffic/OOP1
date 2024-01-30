d3ab9fc945e69611492297f4e2be909ca9627e6f
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2024-01-30 15:57:13 +0200

    Add files via upload

---
+++ Movement.java
@@ -0,0 +1,318 @@
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
+class Movement{
+    private Position from;
+    private Position to;
+    //queue of the pieces that dead because the movement
+    public Queue<Piece> deads = new LinkedList<>();
+    int distance;
+
+    Movement(Position a, Position b){
+        this.from = a;
+        this.to = b;
+        //calculate the distance of the movement
+        if (a.getX() != b.getX())
+            this.distance = Math.abs(a.getX() - b.getX());
+        if (a.getY() != b.getY())
+            this.distance = Math.abs(a.getY() - b.getY());
+
+    }
+
+    public Position getFrom() {
+        return from;
+    }
+    public Position getTo() {
+        return to;
+    }
+
+    public Queue<Piece> getDeads(){
+        return this.deads;
+    }
+
+//    private Piece helpEat(Position pos, String dir, int degree, String direction, GameLogic gameLogic) {
+//        //check if we are on the south border of the board and if there is a piece under us
+//        Position next;
+//        Position nextNext;
+//        if (Objects.equals(dir, "up") && pos.up() != null){
+//            next = pos.up();
+//        }
+//
+//        if (next != null) {
+//            if (next.inTheBoard() && gameLogic.getPieceAtPosition(next) != null) {
+//                //check if the piece one case under our case that is a piece of the other player or null
+//                if (!gameLogic.getPieceAtPosition(next).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+//                    if (!gameLogic.getPieceAtPosition(next).isPawn()) {  // if our neighbor is the king
+//                        this.kingIsDead(gameLogic); // we check if the king is dead
+//                    } else {
+//                        //check if after the piece under our case we have a piece
+//                        if (nextNext != null) {
+//                            if (nextNext.inTheBoard() && gameLogic.getPieceAtPosition(nextNext) != null) {
+//                                if (gameLogic.getPieceAtPosition(nextNext).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+//                                    //eat the piece
+//                                    return this.realEat(pos, next, degree, direction, gameLogic);
+//                                }
+//                            }
+//                            return null;
+//
+//                        }
+//                        //the peace on the up is on the border
+//                        //eat the piece
+//                        return this.realEat(pos, next, degree, direction, gameLogic);
+//                    }
+//                }
+//            }
+//        }
+//        return null;
+//    }
+    /**
+     * The 4 methods check if we eat a piece in the movement and return the piece that we eat
+     */
+    private Piece eatUp(Position pos, GameLogic gameLogic) {
+        /**
+         *  ↑   ↑   ↑
+         */
+        //check if we are on the north border of the board and if there is a piece on the top
+        if (pos.up() != null) {
+            if (pos.up().inTheBoard() && gameLogic.getPieceAtPosition(pos.up()) != null) {
+                //check if the piece one case on top of our case that is a piece of the other player or null
+                if (!gameLogic.getPieceAtPosition(pos.up()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+
+                    if (!gameLogic.getPieceAtPosition(pos.up()).isPawn()) {  // if our neighbor is the king
+                        this.kingIsDead(gameLogic); // we check if the king is dead
+                    }
+
+                    else {
+                        //check if after the piece on top of our case we have a piece
+                        if (pos.up().up() != null) {
+                            if (pos.up().up().inTheBoard() && gameLogic.getPieceAtPosition(pos.up().up()) != null) {
+                                if (gameLogic.getPieceAtPosition(pos.up().up()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+
+                                    //eat the piece
+                                    return this.realEat(pos, pos.up(), -1, "y", gameLogic);
+                                }
+                            }
+                            return null;
+                        }
+                        //the peace on the up is on the border
+                        return this.realEat(pos, pos.up(), -1, "y", gameLogic);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Piece eatDown(Position pos, GameLogic gameLogic) {
+        /**
+         *  ↓  ↓  ↓
+         */
+        //check if we are on the south border of the board and if there is a piece under us
+        if (pos.down() != null) {
+            if (pos.down().inTheBoard() && gameLogic.getPieceAtPosition(pos.down()) != null) {
+                //check if the piece one case under our case that is a piece of the other player or null
+                if (!gameLogic.getPieceAtPosition(pos.down()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                    if (!gameLogic.getPieceAtPosition(pos.down()).isPawn()) {  // if our neighbor is the king
+                        this.kingIsDead(gameLogic); // we check if the king is dead
+                    } else {
+                        //check if after the piece under our case we have a piece
+                        if (pos.down().down() != null) {
+                            if (pos.down().down().inTheBoard() && gameLogic.getPieceAtPosition(pos.down().down()) != null) {
+                                if (gameLogic.getPieceAtPosition(pos.down().down()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                                    //eat the piece
+                                    return this.realEat(pos, pos.down(), 1, "y", gameLogic);
+                                }
+                            }
+                            return null;
+
+                        }
+                        //the peace on the up is on the border
+                        //eat the piece
+                        return this.realEat(pos, pos.down(), 1, "y", gameLogic);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Piece eatRight(Position pos, GameLogic gameLogic) {
+        /**
+         *  →   →   →
+         */
+        //check if we are on the east border of the board and if there is a piece on the right
+        if (pos.right() != null) {
+            if (pos.right().inTheBoard() && gameLogic.getPieceAtPosition(pos.right()) != null) {
+                //check if the piece one case on right of our case that is a piece of the other player or null
+                if (!gameLogic.getPieceAtPosition(pos.right()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                    if (!gameLogic.getPieceAtPosition(pos.right()).isPawn()) {  // if our neighbor is the king
+                        this.kingIsDead(gameLogic); // we check if the king is dead
+                    } else {
+                        //check if after the piece on right of our case we have a piece
+                        if (pos.right().right() != null) {
+                            if (pos.right().right().inTheBoard() && gameLogic.getPieceAtPosition(pos.right().right()) != null) {
+                                if (gameLogic.getPieceAtPosition(pos.right().right()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                                    //eat the piece
+                                    return this.realEat(pos, pos.right(), 1, "x", gameLogic);
+                                }
+                            }
+                            return null;
+
+                        }
+                        //the peace on the right is on the border
+                        //eat the piece
+                        return this.realEat(pos, pos.right(), 1, "x", gameLogic);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Piece eatLeft(Position pos, GameLogic gameLogic) {
+        /**
+         *  ←   ←   ←
+         */
+        //check if we are on the west border of the board and if there is a piece on the left
+        if (pos.left() != null) {
+            if (pos.left().inTheBoard() && gameLogic.getPieceAtPosition(pos.left()) != null) {
+                //check if the piece one case on left of our case that is a piece of the other player or null
+                if (!gameLogic.getPieceAtPosition(pos.left()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                    if (!gameLogic.getPieceAtPosition(pos.left()).isPawn()) {  // if our neighbor is the king
+                        this.kingIsDead(gameLogic); // we check if the king is dead
+                    } else {
+                        //check if after the piece on left of our case we have a piece
+                        if (pos.left().left() != null) {
+                            if (pos.left().left().inTheBoard() && gameLogic.getPieceAtPosition(pos.left().left()) != null) {
+                                if (gameLogic.getPieceAtPosition(pos.left().left()).sameOwner(gameLogic.getPieceAtPosition(pos))) {
+                                    //eat the piece
+                                    return this.realEat(pos, pos.left(), -1, "x", gameLogic);
+                                }
+                            }
+                            return null;
+                        }
+                        //the peace on the right is on the border
+                        //eat the piece
+                        return this.realEat(pos, pos.left(), -1, "x", gameLogic);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /** this function execute the eating */
+    private Piece realEat(Position pos, Position next, int degree, String direction, GameLogic gameLogic){
+        gameLogic.getPieceAtPosition(next).setPositionOfDeath(next);
+        Piece eaten = gameLogic.getPieceAtPosition(next);
+        if (Objects.equals(direction, "x"))
+            gameLogic.board[pos.getX() + degree][pos.getY()] = null;
+        if (Objects.equals(direction, "y"))
+            gameLogic.board[pos.getX()][pos.getY() + degree] = null;
+        return eaten;
+    }
+
+    /** return a queue of all the pieces that we eat in this movement */
+    protected Queue<Piece> eat(Position pos, GameLogic gameLogic) {
+        if (gameLogic.getPieceAtPosition(pos).isPawn()) {
+            Queue<Piece> eaten = new LinkedList<>();
+//            Piece up = this.helpEat(pos, pos.up(), pos.up().up(), -1, "y", gameLogic);
+            Piece up = this.eatUp(pos, gameLogic);
+            if (up != null) eaten.offer(up);
+//            Piece down = this.helpEat(pos, pos.down(), pos.down().down(), 1, "y", gameLogic);
+            Piece down = this.eatDown(pos, gameLogic);
+            if (down != null) eaten.offer(down);
+//            Piece right = this.helpEat(pos, pos.right(), pos.right().right(), 1, "x", gameLogic);
+            Piece right = this.eatRight(pos, gameLogic);
+            if (right != null) eaten.offer(right);
+//            Piece left = this.helpEat(pos, pos.left(), pos.left().left(), -1, "x", gameLogic);
+            Piece left = this.eatLeft(pos, gameLogic);
+            if (left != null) eaten.offer(left);
+            ((Pawn) gameLogic.getPieceAtPosition(pos)).setKills(eaten.size());
+            this.deads.addAll(eaten);
+            return eaten;
+        }
+        return null;
+    }
+
+    /**
+     * This 4 methods check if the king is on danger and if he is dead
+     */
+    private int dangerFromTheNorth(GameLogic gameLogic){
+        if (gameLogic.theKing.getPosition().up() != null) {
+            Position up = new Position(gameLogic.theKing.getPosition().up().getX(), gameLogic.theKing.getPosition().up().getY());
+            if (up.inTheBoard()) {
+                if (gameLogic.getPieceAtPosition(up) != null) {
+                    if (gameLogic.getPieceAtPosition(up).getOwner().isPlayerTwo()) {
+                        return 1; // he is between two enemies
+                    }
+                }
+            }
+            return 0; //not in danger
+        }
+        return 1; //he is between an enemy to the limit of the board
+    }
+    private int dangerFromTheSouth(GameLogic gameLogic){
+        if (gameLogic.theKing.getPosition().down() != null) {
+            Position down = new Position(gameLogic.theKing.getPosition().down().getX(), gameLogic.theKing.getPosition().down().getY());
+            if (down.inTheBoard()) {
+                if (gameLogic.getPieceAtPosition(down) != null) {
+                    if (gameLogic.getPieceAtPosition(down).getOwner().isPlayerTwo()) {
+                        return 1;// he is between two enemies
+                    }
+                }
+            }
+            return 0;//not in danger
+        }
+        return 1; //he is between an enemy to the limit of the board
+    }
+    private int dangerFromTheEast(GameLogic gameLogic){
+        if (gameLogic.theKing.getPosition().right() != null) {
+            Position right = new Position(gameLogic.theKing.getPosition().right().getX(), gameLogic.theKing.getPosition().right().getY());
+            if (right.inTheBoard()) {
+                if (gameLogic.getPieceAtPosition(right) != null) {
+                    if (gameLogic.getPieceAtPosition(right).getOwner().isPlayerTwo()) {
+                        return 1; // he is between two enemies
+                    }
+                }
+            }
+            return 0; //not in danger
+        }
+        return 1; //he is between an enemy to the limit of the board
+    }
+    private int dangerFromTheWest(GameLogic gameLogic){
+        if (gameLogic.theKing.getPosition().left() != null) {
+            Position left = new Position(gameLogic.theKing.getPosition().left().getX(), gameLogic.theKing.getPosition().left().getY());
+            if (left.inTheBoard()) {
+                if (gameLogic.getPieceAtPosition(left) != null) {
+                    if (gameLogic.getPieceAtPosition(left).getOwner().isPlayerTwo()) {
+                        return 1;// he is between two enemies
+                    }
+                }
+            }
+            return 0; //not in danger
+        }
+        return 1; //he is between an enemy to the limit of the board
+    }
+
+    /** this method execute the other methods who check if
+     * the king is dead and if it's true this function kill the king
+     */
+    protected void kingIsDead(GameLogic gameLogic){
+        int a = this.dangerFromTheNorth(gameLogic);
+        int b = this.dangerFromTheSouth(gameLogic);
+        int c = this.dangerFromTheWest(gameLogic);
+        int d = this.dangerFromTheEast(gameLogic);
+        int sum = a + b + c + d;
+        if (sum >= 4) {
+            gameLogic.theKing.dead(); // the king is dead
+        }
+    }
+
+    public String toString(){
+        return "From: " + from.getX()+","+from.getY()+" To: "+to.getX()+","+to.getY();
+    }
+
+}
---
+++ Position.java
@@ -0,0 +1,90 @@
+public class Position {
+
+    private int x;
+    private int y;
+    protected int num_of_stepping = 0;
+
+
+    Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+    private void setX(int x){
+        this.x = x;
+    }
+    private void setY(int y){
+        this.y = y;
+    }
+
+    public void setNum_of_stepping(int num_of_stepping) {
+        this.num_of_stepping += num_of_stepping;
+    }
+    public int getNum_of_stepping(){
+        return this.num_of_stepping;
+    }
+
+    /** this method check if a position is on the game board */
+    public boolean inTheBoard(){
+        if (x >= 0 && x <= 10
+                && y >= 0 && y <= 10){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        String ans = "(" + x + ", " + y + ")";
+        return ans;
+    }
+
+    /**
+     * @return us the position of a neighbor's case and if this is out of
+     * the board return null
+     */
+    public Position up(){
+        Position newPos = new Position(this.getX(), this.getY() - 1);
+        if (newPos.inTheBoard())
+            return newPos;
+        return null;
+    }
+    public Position down(){
+        Position newPos = new Position(this.getX(), this.getY() + 1);
+        if (newPos.inTheBoard())
+            return newPos;
+        return null;
+    }
+    public Position right(){
+        Position newPos = new Position(this.getX() + 1, this.getY());
+        if (newPos.inTheBoard())
+            return newPos;
+        return null;
+    }
+    public Position left(){
+        Position newPos = new Position(this.getX() - 1, this.getY());
+        if (newPos.inTheBoard())
+            return newPos;
+        return null;
+    }
+
+    /** check if the position is the corner of the board */
+    public boolean isCorner(){
+        if (this.getY() == 0 && (this.getX() == 0 || this.getX() == 10))
+            return true;
+        if (this.getY() == 10 && (this.getX() == 0 || this.getX() == 10))
+            return true;
+        return false;
+    }
+    public void setPosition(int x, int y){
+        this.setX(x);
+        this.setY(y);
+    }
+}
